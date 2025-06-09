50da929beadf83eea0c1b5fb4691e6237488981b
Mohd ADIL <shaikhmdadil191@gmail.com>
2025-06-09 12:48:14 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import PyPDF2
+
+pdfFiles=["1.pdf","2.pdf"]
+merger=PyPDF2.PdfMerger()
+for filename in pdfFiles:
+    pdfFile=open(filename,'rb')
+    pdfReader=PyPDF2.PdfFileReader(pdfFile)
+    merger.append(pdfReader)
+pdfFile.close()()
+merger.write('merged.pdf')
